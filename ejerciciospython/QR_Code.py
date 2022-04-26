0d55554727770cf50500101fa9368f09a0b15ac9
Marshall4040 <36226540+IvanCamposNeptune@users.noreply.github.com>
2022-04-26 00:35:10 -0500

    Generación de código QR
    En python 3 con qrcode y pillow.

---
+++ ejerciciospython/QR_Code.py
@@ -0,0 +1,13 @@
+# Codigo para generar un codigo QR
+
+import qrcode # importamos la libreria qrcode
+from PIL import Image # importamos la libreria Image de PIL
+
+cadena = input("Introduce el texto que quieres codificar: ") # pedimos el texto que queremos codificar
+imagen = qrcode.make(cadena) # creamos el codigo QR
+nombre_img= input("Introduce el nombre de la imagen: ")+".png" # pedimos el nombre de la imagen
+archivo_img = open(nombre_img, "wb") # abrimos el archivo en modo escritura binaria
+imagen.save(archivo_img) # guardamos el archivo imagen
+#archivo_img.close() # cerramos el archivo
+ruta_imagen = './' + nombre_img # definimos la ruta de la imagen
+Image.open(ruta_imagen).show() # mostramos la imagen
